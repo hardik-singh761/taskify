72f2dbf2b061b6f9bf2ad5c2aceeacd7150e064c
hardiksingh <officialhardik761@gmail.com>
2024-10-04 21:25:30 +0530

    from scratch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import Header from "./Header";
-import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Header />
+
     </div>
   );
 }
